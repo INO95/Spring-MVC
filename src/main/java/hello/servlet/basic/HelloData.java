c88287f77eba3db9f95f5fe6eccb9e402fd298bc
INO <bih0905@naver.com>
2022-05-18 12:00:30 +0900

    220518 servlet

---
+++ src/main/java/hello/servlet/basic/HelloData.java
@@ -0,0 +1,13 @@
+package hello.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+//자바 빈 접근법 ( 자바 프로퍼티 접근법)
+@Getter @Setter
+public class HelloData {
+
+    private String username;
+    private int age;
+
+}
